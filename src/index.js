0a8a7c9909d9c8a041bf2d76fcbf6a9490a5e633
Andrea Dolci <dolciandrea614@gmail.com>
2024-06-04 09:43:04 +0200

    translations

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Admin', 'Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        socket.emit('message', generateMessage('Admin', `Benvenuto ${user.username}`))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} è entrato nella room!`))
         io.to(user.room).emit('roomData', {
             room: user.room,
             users: getUsersInRoom(user.room)
--- src/utils/users.js
+++ src/utils/users.js
@@ -8,7 +8,7 @@ const addUser = ({ id, username, room }) => {
     // Validate the data
     if (!username || !room) {
         return {
-            error: 'Username and room are required!'
+            error: 'Username e stanza sono obbligatori'
         }
     }
 
@@ -20,7 +20,7 @@ const addUser = ({ id, username, room }) => {
     // Validate username
     if (existingUser) {
         return {
-            error: 'Username is in use!'
+            error: 'Username già in uso'
         }
     }
 
